fc4c54b03917ab8ae5ab64c599b2762043dc1e8b
Leonardo Azocar <leonardoazocar23@gmail.com>
2017-10-27 13:20:03 -0400

    Menu Lateral con enlace a clases y reservas

--- GYM/src/app/app.component.ts
+++ GYM/src/app/app.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild} from '@angular/core';
+import { Platform, Nav} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { InicioPage } from '../pages/inicio/inicio';
+import { ClasesPage } from '../pages/clases/clases';
+import { ReservasPage} from '../pages/reservas/reservas';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+
+  @ViewChild('NAV') nav:Nav;
+  public rootPage:any;
+  public pages: Array<{titulo: string, component: any, icon:string}>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    this.rootPage = InicioPage;
+    this.pages = [
+      {titulo: 'Inicio',component:InicioPage, icon: 'home'},
+      {titulo: 'Clases',component:ClasesPage, icon: 'bicycle'},
+      {titulo: 'Reservas',component:ReservasPage, icon: 'calendar'} ,
+
+    ];
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +31,11 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  goToPage(page){
+    this.nav.setRoot(page);
+  }
+
 }
+
+
