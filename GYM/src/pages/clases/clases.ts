1502f33322ad44a4b35aabc89f13c9733a03e8d8
Leonardo Azocar <leonardoazocar23@gmail.com>
2017-10-28 21:55:05 -0400

    Algunos detalles de las ventanas y agregue el campo duracion de la clase que habia olvidado

--- GYM/src/pages/clases/clases.ts
+++ GYM/src/pages/clases/clases.ts
@@ -18,16 +18,16 @@ import ParticularPage from '../particular/partiular';
 })
 export class ClasesPage {
 /* AUN NO SE SI FUNCIONA -- ESTOY OFFLINE
-public items: Array<{id: number, titulo: string, descripion: string, img:string, instructor:string, fecha: string}>;
+public items: Array<{id: number, titulo: string, descripion: string, img:string, instructor:string, fecha: string, duracion: number}>;
 */
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     /*  AUN NO SE SI FUNCIONA -- ESTOY OFFLINE
     this.items = [
-      {id: 1, titulo:"Yoga", descripion:"sbsdgsdsdfdsfsd dsdssdvsdvsda vsdsd s", img:"../assets/imgs/clasesYoga.jpg", instructor:"Estela Rodriguez", fecha:"24/10/2017 3:00 pm"},
-      {id: 2,titulo:"Spinning",descripion:"sbsdgsdsdfdsfsd dsdssdvsdvsda vsdsd s", img:"../assets/imgs/claseSpinning.jpg", instructor:"Maria Josefa", fecha:"26/10/2017 3:00 pm"},
-      {id: 3,titulo:"Defensa Personal",descripion:"sbsdgsdsdfdsfsd dsdssdvsdvsda vsdsd s", img:"../assets/imgs/clasesDefensaPersonal.jpeg", instructor:"Pedro Alvarez", fecha:"28/10/2017 3:00 pm"},
-      {id: 4,titulo:"Boxeo",descripion:"sbsdgsdsdfdsfsd dsdssdvsdvsda vsdsd s", img:"../assets/imgs/clasesBoxeo.jpeg", instructor:"Jesus Rodriguez", fecha:"30/10/2017 3:00 pm"},
-      {id: 5,titulo:"KickBoxing",descripion:"sbsdgsdsdfdsfsd dsdssdvsdvsda vsdsd s", img:"../assets/imgs/claseKickboxing.png", instructor:"Marcos Rodriguez", fecha:"7/11/2017 3:00 pm"}
+      {id: 1, titulo:"Yoga", descripion:"sbsdgsdsdfdsfsd dsdssdvsdvsda vsdsd s", img:"../assets/imgs/clasesYoga.jpg", instructor:"Estela Rodriguez", fecha:"24/10/2017 3:00 pm", duracion: 60},
+      {id: 2,titulo:"Spinning",descripion:"sbsdgsdsdfdsfsd dsdssdvsdvsda vsdsd s", img:"../assets/imgs/claseSpinning.jpg", instructor:"Maria Josefa", fecha:"26/10/2017 3:00 pm, duracion: 60},
+      {id: 3,titulo:"Defensa Personal",descripion:"sbsdgsdsdfdsfsd dsdssdvsdvsda vsdsd s", img:"../assets/imgs/clasesDefensaPersonal.jpeg", instructor:"Pedro Alvarez", fecha:"28/10/2017 3:00 pm, duracion: 60},
+      {id: 4,titulo:"Boxeo",descripion:"sbsdgsdsdfdsfsd dsdssdvsdvsda vsdsd s", img:"../assets/imgs/clasesBoxeo.jpeg", instructor:"Jesus Rodriguez", fecha:"30/10/2017 3:00 pm, duracion: 60},
+      {id: 5,titulo:"KickBoxing",descripion:"sbsdgsdsdfdsfsd dsdssdvsdvsda vsdsd s", img:"../assets/imgs/claseKickboxing.png", instructor:"Marcos Rodriguez", fecha:"7/11/2017 3:00 pm, duracion: 60}
     ];*/
   }
 
@@ -41,8 +41,8 @@ public items: Array<{id: number, titulo: string, descripion: string, img:string,
 /*   AUN NO SE SI FUNCIONA -- ESTOY OFFLINE
 
   //Deberia de rootear al usuario desde la pagina de clases al darle a la clase de la que quiere ver el detalle
-  goToParticularClase(id: number,titulo: string, descripcion: string img: string, isntructor: string, fecha:string){
-    this.navCtrl.push(particularPage,{id: id, titulo:titulo, img:img, instructor:instructor, fecha:fecha});
+  goToParticularClase(id: number,titulo: string, descripcion: string img: string, isntructor: string, fecha:string, duracion: number){
+    this.navCtrl.push(particularPage,{id: id, titulo:titulo, img:img, instructor:instructor, fecha:fecha, duracion: duracion});
   }
 
   // Deberia de hacer todo el proceso apra reservar una clase
--- GYM/src/pages/reservas/reservas.ts
+++ GYM/src/pages/reservas/reservas.ts
@@ -15,13 +15,13 @@ import { IonicPage, NavController, NavParams, Nav} from 'ionic-angular';
 })
 export class ReservasPage {
 
-  public items: Array<{id: number, titulo:string, img:string, instructor:string, fecha:string}>;
+  public items: Array<{id: number, titulo:string, img:string, instructor:string, fecha:string, duracion:number}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.items = [
-      {id:1, titulo:"Yoga",img:"../assets/imgs/clasesYoga.jpg", instructor:"Estela Rodrigues", fecha:"24/10/2017 3:00 pm"},
-      {id:2, titulo:"Boceo",img:"../assets/imgs/clasesBoxeo.jpg", instructor:"Jesus Rodriguez", fecha:"30/10/2017  3:00 pm"},
-      {id:3, titulo:"KickBoxing",img:"../assets/imgs/claseKickboxing.jpg", instructor:"Marcos Rodriguez", fecha:"7/11/2017 3:00 pm"},
+      {id:1, titulo:"Yoga",img:"../assets/imgs/clasesYoga.jpg", instructor:"Estela Rodrigues", fecha:"24/10/2017 3:00 pm", duracion:60},
+      {id:2, titulo:"Boceo",img:"../assets/imgs/clasesBoxeo.jpg", instructor:"Jesus Rodriguez", fecha:"30/10/2017  3:00 pm", duracion:60},
+      {id:3, titulo:"KickBoxing",img:"../assets/imgs/claseKickboxing.jpg", instructor:"Marcos Rodriguez", fecha:"7/11/2017 3:00 pm", duracion:60},
     ];
   }
 
