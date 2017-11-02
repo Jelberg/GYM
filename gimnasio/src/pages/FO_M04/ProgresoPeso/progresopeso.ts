f45d086945f4e71b2e468ca48d7a25bd18da223f
Jessica Elberg <jessica_elberg@hotmail.com>
2017-11-02 10:55:13 -0400

    Arreglo deunas referencias de Path

--- gimnasio/src/pages/FO_M04/ProgresoPeso/progresopeso.ts
+++ gimnasio/src/pages/FO_M04/ProgresoPeso/progresopeso.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-progresopeso',
+  templateUrl: 'progresopeso.html'
 })
-export class HomePage {
+export class ProgresopesoPage {
 
   constructor(public navCtrl: NavController) {
 
