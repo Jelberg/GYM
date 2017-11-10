91ea1f8aa419fee9ccc40f08008758b97db760f6
Simon Esperanza <simonesperanza3@gmail.com>
2017-11-10 16:15:44 -0400

    Agregado import angular HttpClient

--- gimnasio/src/app/app.module.ts
+++ gimnasio/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import {HttpClientModule } from '@angular/common/http';
 
 //Gráficos
 import { ChartsModule } from 'ng2-charts';
