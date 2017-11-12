fc66c3b072fbf79a99da2316ba9b590a7166a083
Marvian <marvianmontoya@hotmail.com>
2017-11-12 13:56:36 -0300

    empezando a consumir mediddas

--- gimnasio/src/pages/FO_M04/ProgresoMedidas/progresomedidas.ts
+++ gimnasio/src/pages/FO_M04/ProgresoMedidas/progresomedidas.ts
@@ -1,21 +1,33 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { UserServiceProvider } from '../../../providers/user-service/user-service';
+
 
 @IonicPage()
 @Component ({
 
 	selector: 'page-progresomedidas', 
 	templateUrl: 'progresomedidas.html',
-
 })
 
 export class ProgresoMedidasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  			  private userService: UserServiceProvider) {
+
+  	this.getProgresoM()
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProgresoMedidasPage');
   }
+
+  getProcesoM(){
+  	let urlPeticion: string = "F0M04_Progreso_Peso/getProgresoP?id_usuario=1";
+    this.userService.getDato( urlPeticion ).subscribe(data => console.log(data));
+    //luego de => se agrega a la lista, tipo asi: => this.listaProgresos
+
+  }
+
 
 }
