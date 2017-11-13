d9e90398e8f1574fa5ceaf5836ae6bd030d42337
Simon Esperanza <simonesperanza3@gmail.com>
2017-11-13 08:16:35 -0400

    Preprando la grafica de progreso ejercicio con el request

--- gimnasio/src/pages/FO_M04/progreso-ejercicio/progreso-ejercicio.ts
+++ gimnasio/src/pages/FO_M04/progreso-ejercicio/progreso-ejercicio.ts
@@ -20,25 +20,108 @@ import { ListaContactosPage } from '../lista-contactos/lista-contactos';
 export class ProgresoEjercicioPage {
 
   listaRecibida
+  peso = [];
+  repeticion = [];
+  mes = [];
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               private userService : UserServiceProvider) {
-
-                //this.listaRecibida = navParams.get('listaRecibida');
-                this.listaRecibida = navParams.data;
+      this.listaRecibida = navParams.data;
+      this.getDatosEjercicio();
   }
 
-  ionViewDidLoad() {
-    console.log('Ventana progreso ejercicio - Elemento recibido: '+ this.listaRecibida);
+ionViewDidLoad() {
+  console.log(' ////////////////////////// ionviewload ////////////////////////////////////////////////////////////');
+  console.log('///////////////////////// IonViewDidLoad' + this.foo());
   }
 
-  goToListaContactosPage(){
-    this.navCtrl.push(ListaContactosPage);
-  }
+foo(){
+  console.log('#### peso[0]' + this.peso[0]);
+  console.log('#### repeticion[0]' + this.repeticion[0]);
+  console.log('#### mes[0]' + this.mes[0]);
+}
 
-  goToSeleccionarEjercicioPage(){
-    this.navCtrl.push(ListaEjerciciosPage);
+getDatosEjercicio(){
+  let urlPeticion: string = "FOM03_Rutina/getEjercicios6meses?idUsuario=1&ejercicio="+this.listaRecibida;
+   this.userService.getDato( urlPeticion ).subscribe(data => {
+      let i: number = 0;
+      while ( i < data.length ){
+        this.peso[i] = data[i].peso;
+        this.repeticion[i] = data[i].repeticion;
+        this.mes[i] = data[i].mes;
+        i++;
+      }
+      this.foo();
+    }); 
+}
+
+goToListaContactosPage(){
+  this.navCtrl.push(ListaContactosPage);
+}
+
+goToSeleccionarEjercicioPage(){
+  this.navCtrl.push(ListaEjerciciosPage);
+}
+
+public lineChartData:Array<any> = [
+  {data: [65, 59, 80, 81, 56, 55, 40], label: 'Repeticiones'},
+  {data: [28, 48, 40, 19, 86, 27, 90], label: 'Peso'},
+  
+];
+public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+public lineChartOptions:any = {
+  responsive: true
+};
+
+public lineChartColors:Array<any> = [
+  { // grey
+    backgroundColor: 'rgba(148,159,177,0.2)',
+    borderColor: 'rgba(148,159,177,1)',
+    pointBackgroundColor: 'rgba(148,159,177,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+  },
+  { // dark grey
+    backgroundColor: 'rgba(77,83,96,0.2)',
+    borderColor: 'rgba(77,83,96,1)',
+    pointBackgroundColor: 'rgba(77,83,96,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(77,83,96,1)'
+  },
+  { // grey
+    backgroundColor: 'rgba(148,159,177,0.2)',
+    borderColor: 'rgba(148,159,177,1)',
+    pointBackgroundColor: 'rgba(148,159,177,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(148,159,177,0.8)'
   }
+];
+
+public lineChartLegend:boolean = true;
+public lineChartType:string = 'line';
+
+public randomize():void {
+  let _lineChartData:Array<any> = new Array(this.lineChartData.length);
+  for (let i = 0; i < this.lineChartData.length; i++) {
+    _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
+    for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+      _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
+    }
+  }
+  this.lineChartData = _lineChartData;
+}
+
+// events
+public chartClicked(e:any):void {
+  console.log(e);
+}
+
+public chartHovered(e:any):void {
+  console.log(e);
+}
 
 }
