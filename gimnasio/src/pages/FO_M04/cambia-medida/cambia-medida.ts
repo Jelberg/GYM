a846858a1d421c532db7f5ff31ca8b58b77eab2e
Marvian <marvianmontoya@hotmail.com>
2017-11-14 00:16:51 -0300

    Ingresar medidas en progreso

--- gimnasio/src/pages/FO_M04/cambia-medida/cambia-medida.ts
+++ gimnasio/src/pages/FO_M04/cambia-medida/cambia-medida.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { UserServiceProvider } from '../../../providers/user-service/user-service';
+import { ToastController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,14 +10,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CambiaMedidaPage {
 
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams)
-  {
-  	
+  Escapula: number;
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+               private userService: UserServiceProvider, public toastCtrl: ToastController)
+  {  	
 
   }
+
   ionViewDidLoad() 
   {
     console.log('ionViewDidLoad CambiaMedidaPage');
   }
+
+  public cargarMedida():void{
+    console.log( this.Escapula );
+    let urlPeticion = "F0M04_Progreso_Medida/insertaMedidas?id_usuario=1&medida="+this.Escapula;
+    this.userService.postDato( urlPeticion ).subscribe( data => {
+      let i: number = 0;
+      let mensaje: string = "";
+      while ( i < data.lenght ){
+        mensaje = data[i].data;
+      }
+      alert("Medidas agregadas correctamente");
+    });
+  }
+
+  public abrirToast( mensaje: string ):void {
+    let toast = this.toastCtrl.create({
+      message: mensaje,
+      duration: 3000
+    });
+    toast.present();
+  }
+
 }
--- gimnasio/src/providers/user-service/user-service.ts
+++ gimnasio/src/providers/user-service/user-service.ts
@@ -12,11 +12,10 @@ import 'rxjs/add/operator/do';
 */
 @Injectable()
 export class UserServiceProvider {
-<<<<<<< HEAD
-  private URL: string = "http://localhost:8080/ServiciosGimnasio/"
-=======
-  private URL: string = "http://200.84.47.159:8080/web/"
->>>>>>> ba56e77b069e84973e1928f57666c8fd20995308
+
+  //private URL: string = "http://localhost:8080/ServiciosGimnasio/"
+
+  private URL: string = "http://190.79.88.118:8080/web/"
   constructor(/*public http: Http,*/ private http: Http ) {
     console.log('Hello UserServiceProvider Provider');
   }
