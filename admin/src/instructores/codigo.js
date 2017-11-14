b33a0c5182dc6e00ebb0b947b9185c86139817da
Unknown <jorgepintomendes22@gmail.com>
2017-11-14 16:34:12 -0400

    02: Auto stash before merge of "BO_M02" and "origin/BO_M02"

--- admin/src/instructores/codigo.js
+++ admin/src/instructores/codigo.js
@@ -1,21 +1,20 @@
 src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/popper.min.js";
 var url="http://localhost:8080/ServiciosGimnasio/Instruct/getInstruct?correo=marica@gmail.com";
-var correo_busqueda= document.getElementById("text_correo");
-var boton= document.getElementById("boton_consulta");
+var correo_busqueda= document.getElementById("text_lineas");
+var boton= document.getElementById("boton");
 
 boton.addEventListener("click", devolverInstructor);
-
+alert("jeje");
 function devolverInstructor()
 {
-   alert("lola");
+    alert("jeje");
     fetch(url)
     .then((respuesta) => 
     {
-        console.log("mensaje");
-        alert("bff");
+        
         return respuesta.json();
     } ).then((respuesta) => 
-    {alert("bff");
+    {
         console.log(respuesta);
         /*document.getElementById('id').value = respuesta.id;
         document.getElementById('nombre').value = respuesta.nombre;
