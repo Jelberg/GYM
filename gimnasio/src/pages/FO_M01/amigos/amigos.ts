a28eb7278301c9332066e962231dcdaec862e867
Javierhzo <javierhzo1995@gmail.com>
2017-11-13 20:10:23 -0400

    interfaz final

--- gimnasio/src/pages/FO_M01/amigos/amigos.ts
+++ gimnasio/src/pages/FO_M01/amigos/amigos.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 /**
  * Generated class for the AmigosPage page.
  *
@@ -15,11 +14,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AmigosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public alertCtrl: AlertController) {
+      
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AmigosPage');
   }
 
+  agregaramigos(){
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Selecciona amigos a invitar');
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Pablo Escobar',
+      value: 'value1',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Kid Cannabis',
+      value: 'value2'
+    });
+
+    alert.addButton('Cancelar');
+    alert.addButton({
+      text: 'Aceptar',
+      handler: data => {
+      }
+    });
+    alert.present();
+  }
 }
