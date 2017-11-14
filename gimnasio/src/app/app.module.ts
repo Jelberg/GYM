57f5e2c10f70212bf366896e9bc646a320478b6d
Jessica Elberg <jessica_elberg@hotmail.com>
2017-11-14 11:03:40 -0400

    Borre page de valoracion que estaba de mas

--- gimnasio/src/app/app.module.ts
+++ gimnasio/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { HttpModule } from '@angular/http';
-
+import { HttpClientModule } from '@angular/common/http';
 //Gráficos
 import { ChartsModule } from 'ng2-charts';
 
@@ -43,8 +43,7 @@ import { ReservasPage } from '../pages/FO_M05/reservas/reservas';
 import { ClaseParticularPage} from '../pages/FO_M05/clase-particular/clase-particular';
 import { ClasesParticipadasPage} from '../pages/FO_M05/clases-participadas/clases-participadas';
 import { ComentarClasePage} from '../pages/FO_M05/comentar-clase/comentar-clase';
-import { TabCalendarClassPage} from '../pages/FO_M05/tab-calendar-class/tab-calendar-class'
-import { RealizarValoracionPage} from '../pages/FO_M05/realizar-valoracion/realizar-valoracion';
+import { TabCalendarClassPage} from '../pages/FO_M05/tab-calendar-class/tab-calendar-class';
 import { UserServiceProvider } from '../providers/user-service/user-service';
 //FIN M05
 
@@ -84,7 +83,6 @@ import { UserServiceProvider } from '../providers/user-service/user-service';
     ClaseParticularPage,
     ClasesParticipadasPage,
     ComentarClasePage,
-    RealizarValoracionPage,
     TabCalendarClassPage
   ],
   imports: [
@@ -134,7 +132,6 @@ import { UserServiceProvider } from '../providers/user-service/user-service';
     ClasesParticipadasPage,
     ComentarClasePage,
     ReservasPage,
-    RealizarValoracionPage,
     TabCalendarClassPage
   ],
   providers: [
--- gimnasio/src/pages/FO_M05/clases-participadas/clases-participadas.ts
+++ gimnasio/src/pages/FO_M05/clases-participadas/clases-participadas.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ComentarClasePage } from '../comentar-clase/comentar-clase';
+import { UserServiceProvider } from '../../../providers/user-service/user-service';
 
 /**
  * Generated class for the ClasesParticipadasPage page.
@@ -15,20 +16,48 @@ import { ComentarClasePage } from '../comentar-clase/comentar-clase';
   templateUrl: 'clases-participadas.html',
 })
 export class ClasesParticipadasPage {
+
+  criticas: any[] =[];
+  id_user: number;
+
+
   public items:Array<{id:number,titulo:string,instructor:string,fecha:string,hora:string,img:string}>;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.items=[
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public userService: UserServiceProvider
+  ) 
+    {
+ /*   this.items=[
     {id: 1, titulo:"Yoga", img:"../../../assets/imgs/clasesYoga.jpg", instructor:"Estela Rodriguez", fecha:"24/10/2017", hora: "3:00 pm"},
     {id: 2,titulo:"Spinning", img:"../../../assets/imgs/claseSpinning.jpg", instructor:"Maria Josefa", fecha:"26/10/2017", hora: "4:00 pm"},
     {id: 3,titulo:"Defensa Personal", img:"../../../assets/imgs/clasesDefensaPersonal.jpeg", instructor:"Pedro Alvarez", fecha:"28/10/2017", hora: "5:00 pm"}];
+ */ 
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ClasesParticipadasPage');
+   
+  }
+
+  public cargarClasesSinComentar():void{
+    console.log( this.id_user );
+    let url = "F0M05_Critica/sinCritica?id="+3;
+    this.userService.getDato(url)
+        .subscribe( 
+          (data) => {
+      this.criticas = data[''];   
+    },
+    (error) =>{
+      console.error(error);
+    }
+  )
+
   }
 
   goToComentar(){
     this.navCtrl.push(ComentarClasePage);
   }
+
+
 
 }
