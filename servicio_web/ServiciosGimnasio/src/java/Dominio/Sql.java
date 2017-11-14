d7b7cb37f2d607b351076b2c1c3e834fd9712ed4
Unknown <jorgepintomendes22@gmail.com>
2017-11-14 13:55:43 -0400

    arreglando error

--- servicio_web/ServiciosGimnasio/src/java/Dominio/Sql.java
+++ servicio_web/ServiciosGimnasio/src/java/Dominio/Sql.java
@@ -8,9 +8,9 @@ public class Sql {
     private Connection _conn =bdConnect();
     private Statement _st;
     private ResultSet _rs;
-    private static String BD_USER = "GYMUCAB";
-    private static String BD_PASSWORD = "GYMUCAB";
-    private static String BD_URL = "jdbc:postgresql://localhost/GIMNASIOBD";
+    private static String BD_USER = "gymucab"; 
+    private static String BD_PASSWORD = "gymucab"; 
+    private static String BD_URL = "jdbc:postgresql://localhost/gimnasiobd"; 
     private static String BD_CLASS_FOR_NAME = "org.postgresql.Driver";
   /*  
     private static String BD_USER = "postgres";
