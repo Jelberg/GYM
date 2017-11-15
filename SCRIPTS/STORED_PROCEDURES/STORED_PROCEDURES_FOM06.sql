6ca2ef9e774572ebdc32fa627d14a09cb211a477
eavv10 <enrique.vilagut@gmail.com>
2017-11-15 18:14:37 -0400

    Revert "SP seleccionar entrenador"
    This reverts commit 644d72138efb7c314db3c5018dbf4a7d70418253.

--- SCRIPTS/STORED_PROCEDURES/STORED_PROCEDURES_FOM06.sql
+++ SCRIPTS/STORED_PROCEDURES/STORED_PROCEDURES_FOM06.sql
@@ -19,6 +19,7 @@ DECLARE
 	LANGUAGE PLPGSQL;
 
 ---************Esta funcion permitira agregar una reserva******-------
+
 CREATE OR REPLACE fo_m06_agregar_reserva (res_id integer,res_fecha_ini date,res_fecha_fin date,id_usuario int,id_entrenador int)
 RETURNS VOID AS $$
 DECLARE
@@ -38,17 +39,3 @@ BEGIN
 	where reserva_id=res_id;
 	end$$
 	LANGUAGE PLPGSQL; 
-
-
-
----------------Esta funcion devuelve todos los entrenadores en lista-----------------------
-CREATE OR REPLACE FUNCTION fo_m06_get_entrenador (ent_id int)
-RETURNS refcursor AS $$
-DECLARE ref refcursor;
-BEGIN
-	OPEN ref FOR 
-			SELECT ent_nombre AS Nombre, ent_correo AS Email
-			FROM ENTRENADOR 
-	RETURN ref;
-END;
-$$ LANGUAGE PLPGSQL;
