87981f255b6718861b5f716d81be72e406852078
Yesimar <yyhernandez.13@gmail.com>
2017-11-14 21:02:19 -0400

    Agg de funciones en "Horario_Clase" y en store procedure.

--- servicio_web/ServiciosGimnasio/src/java/Servicios/BOM02_Horario_Clase.java
+++ servicio_web/ServiciosGimnasio/src/java/Servicios/BOM02_Horario_Clase.java
@@ -134,7 +134,7 @@ public class BOM02_Horario_Clase {
 
             String query = "select * from bo_m02_inserta_horario_clase(?,?,?,?,?,?,?,?,?,?,?)";
             PreparedStatement st = conn.prepareStatement(query);
-            java.lang.reflect.Type type = new TypeToken<Clase[]>(){}.getType();
+            java.lang.reflect.Type type = new TypeToken<Horario_Clase[]>(){}.getType();
                 st.setInt(1, id_horario_clase);
                 st.setDate(2, fecha);
                 st.setString(3, dia);
@@ -174,7 +174,7 @@ public class BOM02_Horario_Clase {
      * contiene el mensaje de la peticion
      */
     @DELETE
-    @Path("/eliminaClase")
+    @Path("/eliminaHorario_Clase")
     @Produces("application/json")
     public String eliminaClase(@QueryParam("nombreclase") int nombreclase,
                                  @QueryParam("instructor") int instructor,
@@ -196,7 +196,7 @@ public class BOM02_Horario_Clase {
                 put("hora_inicio", hora_inicio );
                 put("hora_fin", hora_fin );
             }});
-                String query = "SELECT bo_m02_elimina_clase(?,?,?,?,?,?,?)";
+                String query = "SELECT bo_m02_elimina_horario_clase(?,?,?,?,?,?,?)";
             PreparedStatement st = conn.prepareStatement(query);
                 st.setInt(1, nombreclase);
                 st.setInt(2, instructor);
@@ -206,7 +206,7 @@ public class BOM02_Horario_Clase {
                 st.setTime(6, hora_inicio);
                 st.setTime(7, hora_fin);
             ResultSet rs = st.executeQuery();
-            response.put("data", "Se elimino la clase");
+            response.put("data", "Se elimino el horario");
         }
         catch(SQLException e) {
             response.put("error", e.getMessage());
@@ -220,5 +220,61 @@ public class BOM02_Horario_Clase {
         }
     }
     
+    /**
+     * Funcion que modifica un horario.
+     * @param nombreclase Nombre de la clase.
+     * @param instructor Identificador del instructor.
+     * @param fecha Fecha de la clase (dd/mm/aaaa).
+     * @param dia Día de la clase (lunes, martes, ... ).
+     * @param capacidad Capacidad de alumnos en la clase.
+     * @param hora_inicio Hora que inicia la clase.
+     * @param hora_fin Hora que finaliza la clase.
+     * @return Devuelve un json con un mensaje al usuario sobre el estatus
+     * de la petición.
+     */
+    @POST
+    @Path("/modificaHorario_Clase")
+    @Produces("application/json")
+    public String modificaClase( @QueryParam ( "nombreclase" ) int nombreclase,
+                                 @QueryParam("instructor") int instructor,
+                                 @QueryParam("fecha") Date fecha,
+                                 @QueryParam("dia") String dia,
+                                 @QueryParam("capacidad") int capacidad,
+                                 @QueryParam("hora_inicio") Time hora_inicio,
+                                 @QueryParam("hora_fin") Time hora_fin){
+        Map<String, String> response = new HashMap<String, String>();
+        try {
+            ValidationWS.validarParametrosNotNull(new HashMap<String, Object>(){ {
+                put("nombreclase", nombreclase);
+                put("instructor", instructor);
+                put("fecha", fecha );
+                put("dia", dia );
+                put("capacidad", capacidad );
+                put("hora_inicio", hora_inicio );
+                put("hora_fin", hora_fin );
+            }});
+            String query = "select * from bo_m02_modifica_horario_clase(?,?,?,?,?,?,?)";
+            PreparedStatement st = conn.prepareStatement(query);
+                st.setInt(1, nombreclase);
+                st.setInt(2, instructor);
+                st.setDate(3, fecha);
+                st.setString(4, dia);
+                st.setInt(5, capacidad);
+                st.setTime(6, hora_inicio);
+                st.setTime(7, hora_fin);
+            st.executeQuery();
+            response.put("data", "Se modificó con éxito");
+        }
+        catch (SQLException e){
+            response.put("error", e.getMessage());
+        }
+        catch (ParameterNullException e) {
+            response.put("error", e.getMessage());
+        }
+        finally {
+            Sql.bdClose(conn);
+            return gson.toJson(response);
+        }
+    } 
     
 }
