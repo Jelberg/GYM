5c0fdf9ca2d8521426b6f7b9bebdea29a48fe0a4
Yesimar <yyhernandez.13@gmail.com>
2017-11-16 23:24:46 -0400

    Pruebas de Clase y Horario_Clase y Modificación de Servicios.

--- servicio_web/ServiciosGimnasio/src/java/Servicios/BOM02_Horario_Clase.java
+++ servicio_web/ServiciosGimnasio/src/java/Servicios/BOM02_Horario_Clase.java
@@ -90,6 +90,48 @@ public class BOM02_Horario_Clase {
     }
     
     /**
+     * @return Devuelve todos los datos de los horarios de clases
+     */
+    @GET
+    @Path("/getListHorario_Clase")
+    @Produces("application/json")
+    public String getListaHorario_Clase(){
+         try{
+            
+            String query = "SELECT * FROM horario_clase;";
+            jsonArray = new ArrayList<>();
+            PreparedStatement st = conn.prepareStatement(query);
+            ResultSet rs = st.executeQuery();
+            
+            //La variable donde se almacena el resultado de la consulta.
+            while(rs.next()){
+                jsonArray.add(new Horario_Clase());
+                jsonArray.get(jsonArray.size() - 1).setId(rs.getInt("HC_ID"));
+                jsonArray.get(jsonArray.size() - 1).setFecha(rs.getDate("HC_FECHA"));
+                jsonArray.get(jsonArray.size() - 1).setDia(rs.getString("HC_DIA"));
+                jsonArray.get(jsonArray.size() - 1).setCapacidad(rs.getInt("HC_CAPACIDAD"));
+                jsonArray.get(jsonArray.size() - 1).setHoraInicio(rs.getTime("HC_HORA_INICIO"));
+                jsonArray.get(jsonArray.size() - 1).setHoraFin(rs.getTime("HC_HORA_FIN"));
+                jsonArray.get(jsonArray.size() - 1).setStatus(rs.getString("HC_STATUS"));
+                jsonArray.get(jsonArray.size() - 1).setDuracion(rs.getInt("HC_DURACION"));
+                jsonArray.get(jsonArray.size() - 1).setNombreclase(rs.getString("fk_clase"));
+                jsonArray.get(jsonArray.size() - 1).setInstructor(rs.getString("fk_instructor"));         
+            }
+            response = gson.toJson(jsonArray);
+        }
+        catch(SQLException e) {
+            response = e.getMessage();
+        }
+        catch (ParameterNullException e) {
+            response = e.getMessage();
+        }
+        finally {
+            Sql.bdClose(conn);
+            return response;
+        }
+    }
+    
+    /**
      * Funcion que es llamada cuando el admin desea insertar un horario de una clase.
      * @param id_horario_clase Identificador de la clase.
      * @param fecha Fecha de la clase (dd/mm/aaaa).
@@ -106,13 +148,12 @@ public class BOM02_Horario_Clase {
     @POST
     @Path("/insertaHorario_Clase")
     @Produces("application/json")
-    public String insertaHorario_Clase(@QueryParam("id_horario_clase") int id_horario_clase,
-                                 @QueryParam("fecha") Date fecha,
+    public String insertaHorario_Clase(@QueryParam("fecha") Date fecha,
                                  @QueryParam("dia") String dia,
                                  @QueryParam("capacidad") int capacidad,
                                  @QueryParam("hora_inicio") Time hora_inicio,
                                  @QueryParam("hora_fin") Time hora_fin,
-                                 @QueryParam("status") int status,
+                                 @QueryParam("status") char status,
                                  @QueryParam("duracion") int duracion,
                                  @QueryParam("nombreclase") int nombreclase,
                                  @QueryParam("instructor") int instructor){
@@ -120,7 +161,6 @@ public class BOM02_Horario_Clase {
         Map<String, String> response = new HashMap<String, String>();
         try {
             ValidationWS.validarParametrosNotNull(new HashMap<String, Object>(){ {
-                put("id_horario_clase", id_horario_clase );
                 put("fecha", fecha );
                 put("dia", dia );
                 put("capacidad", capacidad );
@@ -132,19 +172,18 @@ public class BOM02_Horario_Clase {
                 put("instructor", instructor );
             }});
 
-            String query = "select * from bo_m02_inserta_horario_clase(?,?,?,?,?,?,?,?,?,?,?)";
+            String query = "select * from bo_m02_inserta_horario_clase(?,?,?,?,?,?,?,?,?)";
             PreparedStatement st = conn.prepareStatement(query);
             java.lang.reflect.Type type = new TypeToken<Horario_Clase[]>(){}.getType();
-                st.setInt(1, id_horario_clase);
-                st.setDate(2, fecha);
-                st.setString(3, dia);
-                st.setInt(4, capacidad);
-                st.setTime(5, hora_inicio);
-                st.setTime(6, hora_fin);
-                st.setInt(7, status);
-                st.setInt(8, duracion);
-                st.setInt(10, nombreclase);
-                st.setInt(11, instructor);
+                st.setDate(1, fecha);
+                st.setString(2, dia);
+                st.setInt(3, capacidad);
+                st.setTime(4, hora_inicio);
+                st.setTime(5, hora_fin);
+                st.setInt(6, status);
+                st.setInt(7, duracion);
+                st.setInt(8, nombreclase);
+                st.setInt(9, instructor);
                 st.executeQuery();
             response.put("data", "Se insertó el horario");
         }
@@ -235,7 +274,7 @@ public class BOM02_Horario_Clase {
     @POST
     @Path("/modificaHorario_Clase")
     @Produces("application/json")
-    public String modificaClase( @QueryParam ( "nombreclase" ) int nombreclase,
+    public String modificaHorario_Clase( @QueryParam ( "nombreclase" ) int nombreclase,
                                  @QueryParam("instructor") int instructor,
                                  @QueryParam("fecha") Date fecha,
                                  @QueryParam("dia") String dia,
