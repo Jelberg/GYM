d0e30697ad21ffcc107ed589ae744625d4fcd96b
jorgepm22 <jorgepintomendes22@gmail.com>
2017-11-24 15:05:37 -0400

    Arreglo y mejora de las vistas

--- gimnasio/src/pages/FO_M01/crearusuario/crearusuario.ts
+++ gimnasio/src/pages/FO_M01/crearusuario/crearusuario.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { HomePage } from '../../home/home';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 /**
  * Generated class for the CrearusuarioPage page.
  *
@@ -8,20 +9,71 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+
 @IonicPage()
 @Component({
   selector: 'page-crearusuario',
   templateUrl: 'crearusuario.html',
 })
 export class CrearusuarioPage {
+  public users:any;
+  private cgenero : any;
+  private radioopen : boolean;
   public editar: string = "true";
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public alertCtrl: AlertController) 
+              {
+                this.users = []
+              }
   volver() {
     this.navCtrl.pop();
   }
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CrearusuarioPage');
+      document.getElementById("fechanac").innerHTML = this.fechaActual();
+      document.getElementById("genero").innerHTML = "Seleccionar";
+    
   }
 
+  fechaActual(){
+    var date = new Date().toLocaleDateString();
+    return date;
+  }
+
+  cambiargenero(){
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Genero');
+  
+    alert.addInput({
+      type: 'radio',
+      label: 'Masculino',
+      value: 'Masculino',
+      checked: false
+    });
+  
+    alert.addInput({
+      type: 'radio',
+      label: 'Femenino',
+      value: 'Femenino',
+      checked: false
+    });
+  
+  
+  
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'OK',
+      handler: data => {
+        this.cgenero = data;
+        if (!this.cgenero){
+          
+        }else{
+        this.radioopen = false;
+        document.getElementById("genero").innerHTML= this.cgenero;
+      }}
+    });
+    alert.present().then(() => {
+      this.radioopen=true;
+    })
+  }
 }
