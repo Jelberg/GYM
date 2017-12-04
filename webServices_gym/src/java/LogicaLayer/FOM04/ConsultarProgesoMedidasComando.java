55c07e433f2cafa668e14e6d623ec0cad73b3ec9
Leonardo Azocar <leonardoazocar23@gmail.com>
2017-12-03 20:15:47 -0400

    Agregando Entidades de comandos a webservice

---
+++ webServices_gym/src/java/LogicaLayer/FOM04/ConsultarProgesoMedidasComando.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LogicaLayer.FOM04;
+
+import com.ds1718a.maven_serviciosweb.LogicaLayer.Comando;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class ConsultarProgesoMedidasComando extends Comando {
+    
+    @Override
+    public void ejecutar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
