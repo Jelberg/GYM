4fd26452f6511564686af2596f11c8853c742278
Admin <lugaliguori@gmail.com>
2017-12-10 12:55:49 -0400

    Entidades

---
+++ webServices_gym/src/java/Comun/Dominio/Equipo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Comun.Dominio;
+
+/**
+ *
+ * @author Luis L
+ */
+public class Equipo extends Entidad {
+
+    /**
+     * @return the _id
+     */
+    public int getId() {
+        return _id;
+    }
+
+    /**
+     * @param _id the _id to set
+     */
+    public void setId(int _id) {
+        this._id = _id;
+    }
+
+    /**
+     * @return the _nombre
+     */
+    public String getNombre() {
+        return _nombre;
+    }
+
+    /**
+     * @param _nombre the _nombre to set
+     */
+    public void setNombre(String _nombre) {
+        this._nombre = _nombre;
+    }
+    
+private int _id;
+private String _nombre;
+    
+}
