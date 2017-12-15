862265f29f825ad34f340f2cf4b0621ef45386b3
Unknown <jorgepintomendes22@gmail.com>
2017-12-15 14:55:55 -0400

    correccion de test

--- webServices_gym/test/FOM01/TestLogin.java
+++ webServices_gym/test/FOM01/TestLogin.java
@@ -6,13 +6,18 @@
 package FOM01;
 import Comun.Dominio.FabricaEntidad;
 import Comun.Dominio.Usuario;
+import LogicaLayer.FO1.ComandoEliminaUsuario;
 import LogicaLayer.FO1.IngresarUsuario;
 import LogicaLayer.FabricaComando;
 import ServiciosLayer.FO1_Servicios;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
+import org.junit.Assert;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,6 +29,7 @@ public class TestLogin {
     ResultSet _rs;
     FO1_Servicios _loginServicios;
     Usuario _usuarioDominio;
+    ArrayList<Usuario> _arrayUsu;
 
     
     @Before
@@ -41,6 +47,14 @@ public class TestLogin {
     public void pruebaIniciarSesion(){
         Gson gson = new Gson();
         _loginServicios = new FO1_Servicios();
+        _arrayUsu = new ArrayList<>();
+        _arrayUsu = gson.fromJson(_loginServicios.iniciarSesion("jorgepm", "123456jp"), new TypeToken<List<Usuario>>(){}.getType());
+        assertNotEquals(0, _arrayUsu.get(0).getId());
+    }
+    
+    @Test
+    public void pruebaIngresarUsuario(){
+        _loginServicios = new FO1_Servicios();
         String respuesta = _loginServicios.iniciarSesion("Yesi", "hola");
         assertNotNull( respuesta );
     }
@@ -50,7 +64,10 @@ public class TestLogin {
 
         
         try {
-            
+            Usuario usuario = FabricaEntidad.InstanciaUsuario
+            ("Yesi","hola","Yesimar", "Hernandez",null,"F", "yyhernandez.13@gmail.com", 173,"04265121963",false);        
+            ComandoEliminaUsuario c = FabricaComando.eliminaUsuario("yyhernandez.13@gmail.com");
+            c.ejecutar();
         }catch (NullPointerException e) {
             e.printStackTrace();
         }
