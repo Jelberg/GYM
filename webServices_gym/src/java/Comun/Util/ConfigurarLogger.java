5a9c29fe60430e72c631f8c797731ff23b4a39aa
Simon Esperanza <simonesperanza3@gmail.com>
2017-12-16 17:57:52 -0400

    Agregada Case ConfigurarLogger para manejar el logger del poryecto

---
+++ webServices_gym/src/java/Comun/Util/ConfigurarLogger.java
@@ -0,0 +1,47 @@
+package Comun.Util;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author simon
+ */
+public class ConfigurarLogger {
+    
+    private Logger logr = Logger.getLogger( Logger.GLOBAL_LOGGER_NAME );
+    FileHandler fh;
+    
+    public ConfigurarLogger() {
+        setProperties();
+    }
+    
+    private void setProperties(){
+        try{
+        String ruta = System.getProperty("user.home");
+        
+        //LogManager.getLogManager().reset();
+        logr.setLevel(Level.WARNING);
+        fh = new FileHandler(ruta+"myLogger.log", true);
+        fh.setLevel(Level.WARNING);
+        logr.addHandler(fh);
+            System.out.println(ruta);
+        }
+        catch(IOException e){
+            logr.log(Level.SEVERE, "Error archivo logger", e);
+        }
+    }
+
+    public Logger getLogr() {
+        return logr;
+    }
+
+    public FileHandler getFh() {
+        return fh;
+    }
+    
+    
+}
