c82835b0d87c586770bb07f6a103aefa738b38db
Jessica Elberg <elbergjessica@gmail.com>
2017-12-16 17:51:13 -0400

    Unido ionic y servicio el CRUD del progreso del peso

--- gimnasio/src/pages/FO_M04/cambiar-peso/cambiar-peso.ts
+++ gimnasio/src/pages/FO_M04/cambiar-peso/cambiar-peso.ts
@@ -11,7 +11,7 @@ import { ToastController } from 'ionic-angular';
 export class CambiarPesoPage {
 
   nuevoPeso: number;
-  agregarpeso: any[]=[];
+  peso: any[]=[];
 
   constructor( public navCtrl: NavController, public navParams: NavParams,
                private userService: UserServiceProvider, public toastCtrl: ToastController )
@@ -27,18 +27,18 @@ export class CambiarPesoPage {
    //Metodo que insetar el peso del usuario
    public cargarPeso():void{
     console.log( this.nuevoPeso );
+    this.abrirToast("Peso Agregado");
     let urlPeticion = "F0M04_Progreso_Peso/insertaProgresoPeso?id_usuario=1&peso="+this.nuevoPeso;
     this.userService.getDato(urlPeticion).subscribe(data => {    
       let i: number = 0;
       while ( i < data.length ){
-      this.agregarpeso[i] = data[i];
+      this.peso[i] = data[i];
       i++;}
-    console.log(this.agregarpeso[0]);
+    console.log(this.peso[0]);
   },
   (error) =>{
     console.error(error);
-  }
-)
+  })
     /*  let mensaje:string="";
       let keys = Object.keys(data);
       let key: string ="";
@@ -62,28 +62,38 @@ export class CambiarPesoPage {
   //Metodo que eleminar el peso ingresado en la semana
   public eliminarPeso():void {
     console.log( this.nuevoPeso );
+    this.abrirToast("Peso Eliminado");
     let urlPeticion = "F0M04_Progreso_Peso/eliminarPeso?id_usuario=1";
-    this.userService.deleteDato( urlPeticion ).subscribe( datas => {
-      let mensaje: string = "";
-      let keys = Object.keys(datas);
-      let key: string = "";
-      key = keys[0];
-      mensaje = datas[key];
-      this.abrirToast( mensaje );
-    });
+    this.userService.getDato( urlPeticion ).subscribe( datas => {
+      let i: number = 0;
+      while ( i < datas.length ){
+      this.peso[i] = datas[i];
+      i++;}
+      console.log(this.peso[0]);
+      
+  },
+      (error) =>{
+        //console.log(error);
+      });
   }
   //Metodo para actualizar el peso
   public refrescarPeso():void {
+    this.abrirToast("Peso Actualizado");
     console.log( this.nuevoPeso );
     let urlPeticion = "F0M04_Progreso_Peso/actualizaProgresoPeso?id_usuario=1&peso="+this.nuevoPeso;
-    this.userService.postDato( urlPeticion ).subscribe( datas => {
-      let mensaje: string = "";
-      let keys = Object.keys(datas);
-      let key: string = "";
-      key = keys[0];
-      mensaje = datas[key];
-      this.abrirToast( mensaje );
-
-    });
+    this.userService.getDato( urlPeticion ).subscribe( data => {
+      let i: number = 0;
+      //this.abrirToast("Peso Actualizado");
+      while ( i < data.length ){
+      this.peso[i] = data[i];
+      i++;}
+      console.log(this.peso[0]);
+      
+  },
+    (error) =>{
+      //console.log(error);
+    })
   }
+
+
 }
