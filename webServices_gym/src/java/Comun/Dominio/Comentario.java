2fc9880f0877f5378098f5767de8dca07c1737ae
Jessica Elberg <elbergjessica@gmail.com>
2017-12-17 13:31:34 -0400

    Arreglo de comentario

--- webServices_gym/src/java/Comun/Dominio/Comentario.java
+++ webServices_gym/src/java/Comun/Dominio/Comentario.java
@@ -11,76 +11,63 @@ import java.io.Serializable;
  *
  * @author marvian
  */
-public class Comentario extends Entidad {
-  
-    private String _mensaje;
+public class Comentario extends Entidad implements Serializable {
+    private int _idU;
+    private String _mensajeC;
     private String _NombreUsuario;
     private String _fecha;
     
     public Comentario(){
     }
 
-    public Comentario(int _id, String _mensaje) {
-        super(_id);
-        this._mensaje = _mensaje;
-    }
-
-    public Comentario(String _mensaje) {
-        this._mensaje = _mensaje;
-    }
-    
-    public Comentario(int _id, String _mensaje, String _NombreUsuario, String _fecha) {
-        super(_id);
-        this._mensaje = _mensaje;
-        this._NombreUsuario = _NombreUsuario;
-        this._fecha = _fecha;
-    }
-
-
-    public String getMensaje() {
-        return _mensaje;
-    }
-
-    /**
-     * Modificar mensaje del comentario
-     * @param _mensaje 
-     */
-    public void setMensaje(String _mensaje) {
-        this._mensaje = _mensaje;
-    }
-
-    /**
-     * Obtener nombre del usuario al que le pertenece el comentario
-     * @return 
-     */
-    public String getNombreUsuario() {
-        return _NombreUsuario;
-    }
-
-    /**
-     * Modificar nombre del usuario al que le pertence el comentario
-     * @param _NombreUsuario 
-     */
-    public void setNombreUsuario(String _NombreUsuario) {
-        this._NombreUsuario = _NombreUsuario;
-    }
-
-    /**
-     * Obtener fecha en al que se hizo el comnetario
-     * @return 
-     */
     public String getFecha() {
         return _fecha;
     }
 
-    /**
-     * Modificar fecha en la que se hizo el comentario
-     * @param _fecha 
-     */
     public void setFecha(String _fecha) {
         this._fecha = _fecha;
     }
-
     
-     
+    public String getNombreUsuario() {
+        return _NombreUsuario;
+    }
+
+    public void setNombreUsuario(String _NombreUsuario) {
+        this._NombreUsuario = _NombreUsuario;
+    }
+        
+    public Comentario(int idU, String mensaje ){
+        this._idU = idU;
+        this._mensajeC = mensaje;
+    }
+
+    /**
+     * @return the _id
+     */
+    public int getIdU() {
+        return _idU;
+    }
+
+    /**
+     * @param _id the _id to set
+     */
+    public void setIdU(int _id) {
+        this._idU = _id;
+    }
+
+    /**
+     * @return the _mensaje
+     */
+    public String getMensajeC() {
+        return _mensajeC;
+    }
+
+    /**
+     * @param _mensaje the _mensaje to set
+     */
+    public void setMensajeC(String _mensaje) {
+        this._mensajeC = _mensaje;
+    }    
 }
+
+
